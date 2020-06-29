0d84847e81c8465cd760c04ec19948df316867e2
Toxe <toxe@toxe.net>
2020-06-29 12:47:09 +0200

    Add basic rest api app

---
+++ rest-api/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    return app
