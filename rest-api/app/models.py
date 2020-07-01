e837a710e4c232db6d24a0823d46a52f0d328e0b
Toxe <toxe@toxe.net>
2020-07-01 15:46:47 +0200

    Rearrange some code

--- rest-api/app/models.py
+++ rest-api/app/models.py
@@ -8,15 +8,12 @@ class User(db.Model):
     username = db.Column(db.String(128), index=True, unique=True)
     email    = db.Column(db.String(128), index=True, unique=True)
     password = db.Column(db.String(128))
-
-    def __repr__(self):
-        return "<User:{} {}>".format(self.id, self.username)
-
     def set_password(self, password):
         self.password = generate_password_hash(password)
-
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    def __repr__(self):
+        return "<User:{} {}>".format(self.id, self.username)
 
 
 class UserSchema(Schema):
