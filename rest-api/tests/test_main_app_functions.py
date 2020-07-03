d5d2618833a694c45b30014ee2bb26f2c56c4831
Toxe <toxe@toxe.net>
2020-07-03 15:55:57 +0200

    Add test for create_directory_if_necessary

---
+++ rest-api/tests/test_main_app_functions.py
@@ -0,0 +1,13 @@
+import os
+from app import create_directory_if_necessary
+from tempfile import TemporaryDirectory
+
+
+def test_create_directory_if_necessary():
+    with TemporaryDirectory() as tmp_dir:
+        dirname = os.path.join(tmp_dir, "test")
+        assert os.path.exists(dirname) == False
+        create_directory_if_necessary(dirname)
+        assert os.path.exists(dirname)
+        assert os.path.isdir(dirname)
+        assert os.stat(dirname).st_mode & 0o700 == 0o700
