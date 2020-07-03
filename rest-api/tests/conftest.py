a44e90547081542c0aaecf360ff77a0cb56b9e98
Toxe <toxe@toxe.net>
2020-07-03 15:59:07 +0200

    Create GPXFILES_FOLDER as temporary directory for tests
    Gets automatically removed after each test.

--- rest-api/tests/conftest.py
+++ rest-api/tests/conftest.py
@@ -1,12 +1,17 @@
+import os
 import pytest
 from app import create_app, db
 from config import Config
 from flask_jwt_extended import decode_token
+from tempfile import TemporaryDirectory
 
 
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+    def __init__(self, tmp_dir):
+        self.GPXFILES_FOLDER = os.path.join(tmp_dir, "gpxfiles")
 
 
 class Authentication:
@@ -25,10 +30,11 @@ class Authentication:
 
 @pytest.fixture
 def app():
-    app = create_app(TestConfig)
-    with app.app_context():
-        db.create_all()
-        yield app
+    with TemporaryDirectory() as tmp_dir:
+        app = create_app(TestConfig(tmp_dir))
+        with app.app_context():
+            db.create_all()
+            yield app
 
 
 @pytest.fixture
