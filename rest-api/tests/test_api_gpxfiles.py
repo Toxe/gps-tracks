e95ddf01d6f0d42e6e9a5d78b51cf52b34832e5c
Toxe <toxe@toxe.net>
2020-07-03 15:59:26 +0200

    Test GPX file uploads

--- rest-api/tests/test_api_gpxfiles.py
+++ rest-api/tests/test_api_gpxfiles.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+from app.models import GPXFile, Track
 from tests.example_data_fixtures import example_users, example_gpxfiles
 
 
@@ -25,3 +27,50 @@ def test_get_gpxfiles_for_missing_user(client, example_users, example_gpxfiles):
 
 def test_get_gpxfile_for_missing_user(client, example_users, example_gpxfiles):
     assert client.get("/api/users/99/gpxfiles/1").status_code == 404
+
+
+def test_upload_gpxfile(client, example_users):
+    with open("tests/example.gpx", "rb") as fp:
+        r = client.post("/api/users/1/gpxfiles", data={"file": fp})
+        assert r.status_code == 201
+        assert r.is_json
+        data = r.get_json()
+        assert data["id"] > 0
+        assert "Location" in r.headers
+        assert r.headers.get("Location").endswith("/api/users/1/gpxfiles/{}".format(data["id"]))
+        # make sure database object were created
+        assert len(GPXFile.query.all()) == 1
+        assert len(Track.query.all()) == 1
+
+
+def test_upload_without_gpxfile_fails(client, example_users):
+    r = client.post("/api/users/1/gpxfiles", json={"some": "thing"})
+    assert r.status_code == 400
+    assert r.is_json
+    assert r.get_json().get("message") == "GPX file missing."
+
+
+def test_upload_gpxfile_for_non_existing_user_fails(client):
+    with open("tests/example.gpx", "rb") as fp:
+        r = client.post("/api/users/99/gpxfiles", data={"file": fp})
+        assert r.status_code == 404
+
+
+def test_upload_with_bad_gpxfile_fails(client, example_users):
+    fp = BytesIO("not a gpx file".encode("utf8"))
+    r = client.post("/api/users/1/gpxfiles", data={"file": (fp, "bad.gpx")})
+    assert r.status_code == 400
+    assert r.get_json().get("message") == "Unable to import uploaded GPX file."
+    # make sure no database object were created
+    assert len(GPXFile.query.all()) == 0
+    assert len(Track.query.all()) == 0
+
+
+def test_upload_with_bad_xml_gpxfile_fails(client, example_users):
+    fp = BytesIO('<?xml version="1.0" encoding="UTF-8"?><gpx version="1.0">'.encode("utf8"))
+    r = client.post("/api/users/1/gpxfiles", data={"file": (fp, "bad.gpx")})
+    assert r.status_code == 400
+    assert r.get_json().get("message") == "Unable to import uploaded GPX file."
+    # make sure no database object were created
+    assert len(GPXFile.query.all()) == 0
+    assert len(Track.query.all()) == 0
