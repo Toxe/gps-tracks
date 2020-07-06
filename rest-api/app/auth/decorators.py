4ab00e8537ef599fbba8bb320e76e85bd1ee32a7
Toxe <toxe@toxe.net>
2020-07-06 17:11:34 +0200

    Add jwt_and_matching_user_id_required custom decorator

---
+++ rest-api/app/auth/decorators.py
@@ -0,0 +1,15 @@
+from functools import wraps
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
+from app.errors.handlers import error_response
+
+
+# Custom decorator that checks if a param "user_id" is equal to the JWT user id.
+def jwt_and_matching_user_id_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        user_id = kwargs.get("user_id")
+        if user_id != get_jwt_identity():
+            return error_response(403, "Access to user resource denied.")
+        return fn(*args, **kwargs)
+    return wrapper
