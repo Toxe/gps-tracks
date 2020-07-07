f4ceb61233daaa8253877a9c44716877f21c96ff
Toxe <toxe@toxe.net>
2020-07-07 12:50:55 +0200

    Tracks API functions need login

--- rest-api/app/api/tracks.py
+++ rest-api/app/api/tracks.py
@@ -2,15 +2,19 @@ from flask import jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.api import bp
 from app.models import User, Track, track_schema
+from app.auth.decorators import jwt_and_matching_user_id_required
+from app.errors.handlers import error_response
 
 
 @bp.route("/users/<int:user_id>/tracks", methods=["GET"])
+@jwt_and_matching_user_id_required
 def get_user_tracks(user_id):
     user = User.query.get_or_404(user_id)
     return jsonify(track_schema.dump(user.tracks.all(), many=True))
 
 
 @bp.route("/users/<int:user_id>/tracks/<int:track_id>", methods=["GET"])
+@jwt_and_matching_user_id_required
 def get_user_track(user_id, track_id):
     user = User.query.get_or_404(user_id)
     track = user.tracks.filter_by(id=track_id).first_or_404()
--- rest-api/tests/test_api_tracks.py
+++ rest-api/tests/test_api_tracks.py
@@ -1,27 +1,30 @@
 from tests.example_data_fixtures import example_users, example_tracks
 
 
-def test_get_tracks(client, example_users, example_tracks):
-    r = client.get("/api/users/1/tracks")
+def test_get_tracks(client, auth, example_users, example_tracks):
+    auth.login("user1@example.com", "password1")
+    r = client.get("/api/users/{}/tracks".format(auth.id), headers=auth.headers)
     assert r.status_code == 200
     assert r.is_json
     assert len(r.get_json()) == 2
 
 
-def test_get_track(client, example_users, example_tracks):
-    r = client.get("/api/users/1/tracks/1")
+def test_get_track(client, auth, example_users, example_tracks):
+    auth.login("user1@example.com", "password1")
+    r = client.get("/api/users/{}/tracks/1".format(auth.id), headers=auth.headers)
     assert r.status_code == 200
     assert r.is_json
     assert r.get_json().get("id") == 1
 
 
-def test_get_missing_track(client, example_users, example_tracks):
-    assert client.get("/api/users/1/tracks/99").status_code == 404
+def test_get_missing_track(client, auth, example_users, example_tracks):
+    auth.login("user1@example.com", "password1")
+    assert client.get("/api/users/{}/tracks/99".format(auth.id), headers=auth.headers).status_code == 404
 
 
-def test_get_tracks_for_missing_user(client, example_users, example_tracks):
-    assert client.get("/api/users/99/tracks").status_code == 404
+def test_get_tracks_without_login(client, example_users, example_tracks):
+    assert client.get("/api/users/1/tracks").status_code == 401
 
 
-def test_get_track_for_missing_user(client, example_users, example_tracks):
-    assert client.get("/api/users/99/tracks/1").status_code == 404
+def test_get_track_without_login(client, example_users, example_tracks):
+    assert client.get("/api/users/1/tracks/1").status_code == 401
