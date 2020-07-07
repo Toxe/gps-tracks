3bcabe84d74a347c1eac29f73b4bb55a7e32fb05
Toxe <toxe@toxe.net>
2020-07-07 12:55:05 +0200

    Test tracks API forbidden access

--- rest-api/tests/test_api_tracks.py
+++ rest-api/tests/test_api_tracks.py
@@ -28,3 +28,19 @@ def test_get_tracks_without_login(client, example_users, example_tracks):
 
 def test_get_track_without_login(client, example_users, example_tracks):
     assert client.get("/api/users/1/tracks/1").status_code == 401
+
+
+def test_get_tracks_for_different_user_is_forbidden(client, auth, example_users, example_tracks):
+    auth.login("user1@example.com", "password1")
+    r = client.get("/api/users/2/tracks".format(auth.id), headers=auth.headers)
+    assert r.status_code == 403
+    assert r.is_json
+    assert r.get_json().get("message") == "Access to user resource denied."
+
+
+def test_get_track_for_different_user_is_forbidden(client, auth, example_users, example_tracks):
+    auth.login("user1@example.com", "password1")
+    r = client.get("/api/users/2/tracks/1".format(auth.id), headers=auth.headers)
+    assert r.status_code == 403
+    assert r.is_json
+    assert r.get_json().get("message") == "Access to user resource denied."
