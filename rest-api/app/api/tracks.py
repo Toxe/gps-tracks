e5f05c210463e4cd508efe3208d7ffdeb85e0c42
Toxe <toxe@toxe.net>
2020-07-08 15:14:18 +0200

    Add API route to delete user tracks

--- rest-api/app/api/tracks.py
+++ rest-api/app/api/tracks.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from app import db
 from app.api import bp
 from app.models import User, Track, track_schema
 from app.auth.decorators import jwt_and_matching_user_id_required
@@ -19,3 +20,13 @@ def get_user_track(user_id, track_id):
     user = User.query.get_or_404(user_id)
     track = user.tracks.filter_by(id=track_id).first_or_404()
     return jsonify(track_schema.dump(track))
+
+
+@bp.route("/users/<int:user_id>/tracks/<int:track_id>", methods=["DELETE"])
+@jwt_and_matching_user_id_required
+def delete_user_track(user_id, track_id):
+    user = User.query.get_or_404(user_id)
+    track = user.tracks.filter_by(id=track_id).first_or_404()
+    db.session.delete(track)
+    db.session.commit()
+    return "", 204
--- rest-api/tests/test_api_tracks.py
+++ rest-api/tests/test_api_tracks.py
@@ -1,3 +1,4 @@
+from app.models import User
 from tests.example_data_fixtures import example_users, example_tracks
 
 
@@ -41,6 +42,33 @@ def test_get_tracks_for_different_user_is_forbidden(client, auth, example_users,
 def test_get_track_for_different_user_is_forbidden(client, auth, example_users, example_tracks):
     auth.login("user1@example.com", "password1")
     r = client.get("/api/users/2/tracks/1".format(auth.id), headers=auth.headers)
+    assert r.status_code == 403
+    assert r.is_json
+    assert r.get_json().get("message") == "Access to user resource denied."
+
+
+def test_delete_track(client, auth, example_users, example_tracks):
+    auth.login("user1@example.com", "password1")
+    user = User.query.get(auth.id)
+    assert len(user.tracks.all()) == 3
+    r = client.delete("/api/users/{}/tracks/1".format(auth.id), headers=auth.headers)
+    assert r.status_code == 204
+    assert len(user.tracks.all()) == 2
+
+
+def test_delete_missing_track(client, auth, example_users, example_tracks):
+    auth.login("user1@example.com", "password1")
+    r = client.delete("/api/users/{}/tracks/99".format(auth.id), headers=auth.headers)
+    assert r.status_code == 404
+
+
+def test_delete_track_without_login(client, example_users, example_tracks):
+    assert client.delete("/api/users/1/tracks/1").status_code == 401
+
+
+def test_delete_track_for_different_user_is_forbidden(client, auth, example_users, example_tracks):
+    auth.login("user1@example.com", "password1")
+    r = client.get("/api/users/2/tracks/3".format(auth.id), headers=auth.headers)
     assert r.status_code == 403
     assert r.is_json
     assert r.get_json().get("message") == "Access to user resource denied."
