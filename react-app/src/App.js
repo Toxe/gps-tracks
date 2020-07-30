3f8a3078ffd234b1093e0f898de371d3ab72eff0
Toxe <toxe@toxe.net>
2020-07-30 13:01:37 +0200

    Add Drawer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
-import { makeStyles, fade } from "@material-ui/core/styles";
-import { AppBar, Toolbar, Typography, TextField, InputAdornment } from "@material-ui/core";
+import { makeStyles, useTheme, fade } from "@material-ui/core/styles";
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    TextField,
+    InputAdornment,
+    Hidden,
+    Drawer,
+    Divider,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    IconButton,
+} from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import SearchIcon from "@material-ui/icons/Search";
+import FolderIcon from "@material-ui/icons/Folder";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
     spacer: { flexGrow: 1.0 },
+    toolbar: theme.mixins.toolbar,
+    appBar: {
+        [theme.breakpoints.up("sm")]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+        },
+    },
     searchField: {
         marginLeft: theme.spacing(3),
         borderRadius: theme.shape.borderRadius,
@@ -14,18 +39,78 @@ const useStyles = makeStyles((theme) => ({
             backgroundColor: fade(theme.palette.common.white, 0.25),
         },
     },
-    userName: {
-        marginRight: theme.spacing(3),
+    userName: { marginRight: theme.spacing(3) },
+    menuButton: { marginRight: theme.spacing(2) },
+    drawerPaper: { width: drawerWidth },
+    drawer: {
+        [theme.breakpoints.up("sm")]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    content: {
+        flexGrow: 1.0,
+        padding: theme.spacing(3),
     },
 }));
 
 export default function App() {
     const classes = useStyles();
+    const theme = useTheme();
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawer = (
+        <div>
+            <div className={classes.toolbar} />
+            <List>
+                <ListItem button>
+                    <ListItemIcon>
+                        <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="253 Tracks" />
+                </ListItem>
+            </List>
+            <Divider />
+            <List>
+                <ListItem button>
+                    <ListItemIcon>
+                        <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="2020 (53)" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="2019 (17)" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="2018 (86)" />
+                </ListItem>
+            </List>
+        </div>
+    );
 
     return (
         <>
-            <AppBar position="static">
+            <AppBar position="fixed" className={classes.appBar}>
                 <Toolbar>
+                    <Hidden smUp implementation="css">
+                        <IconButton
+                            color="inherit"
+                            edge="start"
+                            onClick={handleDrawerToggle}
+                            className={classes.menuButton}>
+                            <MenuIcon />
+                        </IconButton>
+                    </Hidden>
                     <Typography variant="h6" noWrap>
                         GPS Tracks
                     </Typography>
@@ -49,7 +134,28 @@ export default function App() {
                     <GitHubIcon></GitHubIcon>
                 </Toolbar>
             </AppBar>
-            Content
+            <nav className={classes.drawer}>
+                <Hidden smUp implementation="css">
+                    <Drawer
+                        variant="temporary"
+                        classes={{ paper: classes.drawerPaper }}
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        anchor={theme.direction === "rtl" ? "right" : "left"}
+                        ModalProps={{ keepMounted: true }}>
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+                <Hidden xsDown implementation="css">
+                    <Drawer variant="permanent" open classes={{ paper: classes.drawerPaper }}>
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+            </nav>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Typography paragraph>Content.</Typography>
+            </main>
         </>
     );
 }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import "fontsource-roboto";
 import { CssBaseline } from "@material-ui/core";
 import App from "./App";
