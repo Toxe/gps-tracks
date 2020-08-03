358edf718d08dfac28ee514df63caf6d5f0d7520
Toxe <toxe@toxe.net>
2020-08-03 15:26:40 +0200

    Add year and activity filter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,28 +2,33 @@ import React, { useState } from "react";
 import { makeStyles, useTheme, fade } from "@material-ui/core/styles";
 import {
     AppBar,
-    Toolbar,
-    Typography,
-    TextField,
-    InputAdornment,
-    Hidden,
-    Drawer,
+    Box,
+    Button,
     Divider,
+    Drawer,
+    FormControl,
+    Hidden,
+    IconButton,
+    InputAdornment,
+    InputLabel,
     List,
     ListItem,
     ListItemIcon,
     ListItemText,
-    IconButton,
-    Button,
     Menu,
     MenuItem,
-    Box,
+    Select,
+    TextField,
+    Toolbar,
+    Typography,
 } from "@material-ui/core";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import SearchIcon from "@material-ui/icons/Search";
-import FolderIcon from "@material-ui/icons/Folder";
-import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import DirectionsBikeIcon from "@material-ui/icons/DirectionsBike";
+import DirectionsWalkIcon from "@material-ui/icons/DirectionsWalk";
+import FolderIcon from "@material-ui/icons/Folder";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
 
 const drawerWidth = 240;
 
@@ -49,6 +54,13 @@ const useStyles = makeStyles((theme) => ({
             flexShrink: 0,
         },
     },
+    filterForm: {
+        marginRight: 20,
+    },
+    filterFormSelect: {
+        minWidth: 80,
+        minHeight: 37,
+    },
 }));
 
 export default function App() {
@@ -56,6 +68,8 @@ export default function App() {
     const theme = useTheme();
     const [mobileOpen, setMobileOpen] = useState(false);
     const [userMenuAnchorEl, setUserMenuAnchorEl] = useState(null);
+    const [activityFilter, setActivityFilter] = useState("");
+    const [yearFilter, setYearFilter] = useState("");
 
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
@@ -67,6 +81,14 @@ export default function App() {
 
     const handleUserMenuClose = () => {
         setUserMenuAnchorEl(null);
+    };
+
+    const handleChangeActivityFilter = (e) => {
+        setActivityFilter(e.target.value);
+    };
+
+    const handleChangeYearFilter = (e) => {
+        setYearFilter(e.target.value);
     };
 
     const drawer = (
@@ -186,7 +208,44 @@ export default function App() {
             </nav>
             <Box flexGrow={1} p={3}>
                 <div className={classes.toolbar} />
-                <Typography paragraph>Content.</Typography>
+                <Typography variant="h5">253 Tracks</Typography>
+                <Box>
+                    <FormControl className={classes.filterForm}>
+                        <InputLabel id="activity-filter-select-label">Activity</InputLabel>
+                        <Select
+                            className={classes.filterFormSelect}
+                            labelId="activity-filter-select-label"
+                            id="activity-filter-select"
+                            value={activityFilter}
+                            onChange={handleChangeActivityFilter}>
+                            <MenuItem value="all">All</MenuItem>
+                            <MenuItem value="bike">
+                                <ListItemIcon>
+                                    <DirectionsBikeIcon fontSize="small" />
+                                </ListItemIcon>
+                            </MenuItem>
+                            <MenuItem value="hiking">
+                                <ListItemIcon>
+                                    <DirectionsWalkIcon fontSize="small" />
+                                </ListItemIcon>
+                            </MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel id="year-filter-select-label">Year</InputLabel>
+                        <Select
+                            className={classes.filterFormSelect}
+                            labelId="year-filter-select-label"
+                            id="year-filter-select"
+                            value={yearFilter}
+                            onChange={handleChangeYearFilter}>
+                            <MenuItem value="all">All</MenuItem>
+                            <MenuItem value="2020">2020</MenuItem>
+                            <MenuItem value="2019">2019</MenuItem>
+                            <MenuItem value="2018">2018</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
             </Box>
         </>
     );
