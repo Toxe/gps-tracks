f81f21c5db576904c8b983f1cb47599c4bef53c7
Toxe <toxe@toxe.net>
2020-08-05 14:54:18 +0200

    Update Roboto font to 3.0.3

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "fontsource-roboto";
+import "fontsource-roboto/300.css";
+import "fontsource-roboto/400.css";
+import "fontsource-roboto/500.css";
+import "fontsource-roboto/700.css";
 import { CssBaseline } from "@material-ui/core";
 import App from "./App";
 
