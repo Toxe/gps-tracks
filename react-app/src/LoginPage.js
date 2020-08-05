5c7fe9a5f5c7523d3935037bcabae8f4d7a3c7b5
Toxe <toxe@toxe.net>
2020-08-05 19:42:04 +0200

    Add simple login form

--- react-app/src/LoginPage.js
+++ react-app/src/LoginPage.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box } from "@material-ui/core";
 import MainHeader from "./MainHeader";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles((theme) => ({
     toolbar: theme.mixins.toolbar,
+    container: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    form: {
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
 export default function MainPage() {
@@ -13,9 +28,37 @@ export default function MainPage() {
     return (
         <>
             <MainHeader />
-            <Box flexGrow={1} p={3}>
+            <Container maxWidth="xs" className={classes.container}>
                 <div className={classes.toolbar} />
-            </Box>
+                <Typography variant="h4">Sign in</Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                        Sign In
+                    </Button>
+                </form>
+            </Container>
         </>
     );
 }
