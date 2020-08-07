84d20308d6ddf0cd37767f4ee4e080f76baed052
Toxe <toxe@toxe.net>
2020-08-07 18:01:59 +0200

    Show request error

--- react-app/src/Auth/AuthInfo.js
+++ react-app/src/Auth/AuthInfo.js
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 import TokenInfo from "./TokenInfo";
 import { CurrentUserContext } from "./CurrentUserContext";
+import RequestError from "../RequestError";
 
 const useStyles = makeStyles(() => ({
     authInfo: {
@@ -17,9 +18,19 @@ const useStyles = makeStyles(() => ({
 export default function AuthInfo({ handleRefresh }) {
     const classes = useStyles();
     const { currentUserId } = useContext(CurrentUserContext);
+    const [requestError, setRequestError] = useState(null);
 
     if (currentUserId === 0)
         return null;
+
+    const handleRefreshButtonClick = async () => {
+        try {
+            await handleRefresh();
+            setRequestError(null);
+        } catch (error) {
+            setRequestError(<RequestError error={error} handleClose={() => setRequestError(null)} />);
+        }
+    }
 
     return (
         <div className={classes.authInfo}>
@@ -27,8 +38,9 @@ export default function AuthInfo({ handleRefresh }) {
             <TokenInfo tokenName="access_token" />
             <TokenInfo tokenName="refresh_token" />
             <div>
-                <button type="button" onClick={handleRefresh}>Refresh token</button>
+                <button type="button" onClick={handleRefreshButtonClick}>Refresh token</button>
             </div>
+            {requestError}
         </div>
     );
 }
