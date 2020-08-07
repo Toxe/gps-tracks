9667089c80f4a34ac176a3c370ca63b870e68b7f
Toxe <toxe@toxe.net>
2020-08-07 18:24:15 +0200

    Add RequestError component

---
+++ react-app/src/RequestError.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    requestError: {
+        color: "white",
+        background: "red",
+        padding: theme.spacing(1),
+    },
+}));
+
+export default function RequestError({ error, handleClose }) {
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.requestError}>
+            <Typography variant="body1">Request error: {error.message}</Typography>
+            {error.response && <Typography variant="body2">Server response: {error.response.status} {error.response.data.error}</Typography>}
+            {error.response && error.response.data.message && <Typography variant="body2">{JSON.stringify(error.response.data.message, null, "  ")}</Typography>}
+            <div>
+                <button type="button" onClick={handleClose}>dismiss</button>
+            </div>
+        </Paper>
+    );
+}
