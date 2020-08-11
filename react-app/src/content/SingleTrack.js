daaeaf60e4cf3ac87aeaa516c2540577734d3344
Toxe <toxe@toxe.net>
2020-08-11 16:56:38 +0200

    Show example track on single track page

--- react-app/src/content/SingleTrack.js
+++ react-app/src/content/SingleTrack.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import ExampleTrack from "./ExampleTrack";
 
 export default function SingleTrack() {
     const { trackId } = useParams();
 
-    return <div>SingleTrack #{trackId}</div>;
+    return <ExampleTrack id={trackId} activity="bike" />;
 }
