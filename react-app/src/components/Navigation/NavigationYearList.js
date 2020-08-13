36d994e780c95381a3db13ceec4316c35ca95d02
Toxe <toxe@toxe.net>
2020-08-13 21:19:29 +0200

    Filter by year from Navigation

--- react-app/src/components/Navigation/NavigationYearList.js
+++ react-app/src/components/Navigation/NavigationYearList.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Divider, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import FolderIcon from "@material-ui/icons/Folder";
 
 export default function NavigationYearList({ countedYears }) {
+    const navigate = useNavigate();
     const years = [...countedYears.keys()].sort((a, b) => b - a);
+
+    const handleClick = (year) => {
+        navigate(`/tracks?y=${year}`);
+    };
 
     return (
         <>
             <List>
                 {years.map((y) => (
-                    <ListItem key={y} button>
+                    <ListItem key={y} button onClick={() => handleClick(y)}>
                         <ListItemIcon>
                             <FolderIcon />
                         </ListItemIcon>
