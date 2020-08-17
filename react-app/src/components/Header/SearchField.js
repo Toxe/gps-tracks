e13d6b32930727b2fe7c04072ea3a440bd258783
Toxe <toxe@toxe.net>
2020-08-17 17:53:26 +0200

    Update search field styling

--- react-app/src/components/Header/SearchField.js
+++ react-app/src/components/Header/SearchField.js
@@ -1,17 +1,45 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { makeStyles, fade } from "@material-ui/core/styles";
-import { InputAdornment, TextField } from "@material-ui/core";
+import { InputBase } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles((theme) => ({
-    searchField: {
-        marginLeft: theme.spacing(3),
-        marginRight: theme.spacing(2),
+    search: {
+        position: "relative",
         borderRadius: theme.shape.borderRadius,
         backgroundColor: fade(theme.palette.common.white, 0.15),
         "&:hover": {
             backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+            marginLeft: theme.spacing(3),
+            width: "auto",
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: "100%",
+        position: "absolute",
+        pointerEvents: "none",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    inputRoot: {
+        color: "inherit",
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+            width: "20ch",
         },
     },
 }));
@@ -21,18 +49,17 @@ export default function SearchField() {
     const classes = useStyles();
 
     return (
-        <TextField
-            variant="outlined"
-            size="small"
-            className={classes.searchField}
-            placeholder={t("search_placeholder")}
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        <SearchIcon />
-                    </InputAdornment>
-                ),
-            }}
-        />
+        <div className={classes.search}>
+            <div className={classes.searchIcon}>
+                <SearchIcon />
+            </div>
+            <InputBase
+                placeholder={t("search_placeholder")}
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+            />
+        </div>
     );
 }
