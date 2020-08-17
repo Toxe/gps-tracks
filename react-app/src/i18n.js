c7455fc350f04bb0a1f4a87db620d9b2f1452c11
Toxe <toxe@toxe.net>
2020-08-17 19:27:23 +0200

    Disable i18n debug mode

--- react-app/src/i18n.js
+++ react-app/src/i18n.js
@@ -8,7 +8,7 @@ i18n.use(Backend)
     .use(initReactI18next)
     .init({
         keySeparator: false,
-        debug: true,
+        // debug: true,
         interpolation: {
             escapeValue: false,
         },
