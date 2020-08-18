6e4fffed7b0140891ab3ee8e5719ced809e1ba7b
Toxe <toxe@toxe.net>
2020-08-18 17:46:52 +0200

    Debounce search update

--- react-app/src/components/TracksSearch/SearchField.js
+++ react-app/src/components/TracksSearch/SearchField.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { useSearchParams } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { InputAdornment, TextField } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import { useTracksSearch } from "./TracksSearchProvider";
+import debounce from "lodash/debounce";
 
 const useStyles = makeStyles((theme) => ({
     searchField: {
@@ -23,14 +24,30 @@ export default function SearchField() {
     const classes = useStyles();
     const [searchParams, setSearchParams] = useSearchParams();
     const { searchText, setSearchText } = useTracksSearch();
+    const [searchFieldContent, setSearchFieldContent] = useState("");
 
+    // update URL search param after a short delay once the user stopped typing
+    const debouncedUpdateSearch = useCallback(
+        debounce((text) => {
+            searchParams.set("search", text);
+            setSearchParams(searchParams);
+        }, 300),
+        []
+    );
+
+    // initialize searchText from URL param
     useEffect(() => {
         setSearchText(getSearchParam(searchParams, "search", ""));
     }, [searchParams, setSearchText]);
 
+    // initialize textfield content from searchText
+    useEffect(() => {
+        setSearchFieldContent(searchText);
+    }, [searchText]);
+
     const handleChangeSearch = (e) => {
-        searchParams.set("search", e.target.value);
-        setSearchParams(searchParams);
+        setSearchFieldContent(e.target.value);
+        debouncedUpdateSearch(e.target.value);
     };
 
     return (
@@ -46,7 +63,7 @@ export default function SearchField() {
                     </InputAdornment>
                 ),
             }}
-            value={searchText}
+            value={searchFieldContent}
             onChange={handleChangeSearch}
         />
     );
