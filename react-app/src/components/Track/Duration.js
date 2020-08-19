c73d2e7c04488eb40dc57725538ecf5f0d6ed81e
Toxe <toxe@toxe.net>
2020-08-19 15:10:41 +0200

    Format duration as hours and minutes

--- react-app/src/components/Track/Duration.js
+++ react-app/src/components/Track/Duration.js
@@ -2,5 +2,7 @@ import React from "react";
 import { Typography } from "@material-ui/core";
 
 export default function Duration({ duration }) {
-    return <Typography variant="body1">{duration} m</Typography>;
+    const h = new Date(duration * 1000.0).toISOString().substr(11, 5);
+
+    return <Typography variant="body1">{h}</Typography>;
 }
