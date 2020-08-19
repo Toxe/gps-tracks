5d9d2ac8df9e59738aa79c2677a89e1d00d21de1
Toxe <toxe@toxe.net>
2020-08-19 18:31:15 +0200

    Format track date with current locale

--- react-app/src/components/Track/FormattedDate.js
+++ react-app/src/components/Track/FormattedDate.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
 
 export default function FormattedDate({ time }) {
-    return <Typography variant="body2" color="textSecondary">{time}</Typography>;
+    const { i18n } = useTranslation();
+    const options = { year: "numeric", month: "long", day: "numeric" };
+
+    return (
+        <Typography variant="body2" color="textSecondary">
+            {new Date(time).toLocaleDateString(i18n.language, options)}
+        </Typography>
+    );
 }
