9e1a8c33af4bc8660ac388213ad6f63ecc3252fe
Toxe <toxe@toxe.net>
2020-08-19 14:53:26 +0200

    Show speed as km/h

--- react-app/src/components/Track/Speed.js
+++ react-app/src/components/Track/Speed.js
@@ -2,5 +2,7 @@ import React from "react";
 import { Typography } from "@material-ui/core";
 
 export default function Speed({ speed }) {
-    return <Typography variant="body1">{speed} m</Typography>;
+    const kph = Number.parseFloat(speed);
+
+    return <Typography variant="body1">{kph.toFixed(2)} km/h</Typography>;
 }
