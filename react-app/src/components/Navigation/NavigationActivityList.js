f672ad9bd0c16b87b7b23d2e127c19a0a17ba25a
Toxe <toxe@toxe.net>
2020-08-20 13:07:45 +0200

    Use ActivityMode enum

--- react-app/src/components/Navigation/NavigationActivityList.js
+++ react-app/src/components/Navigation/NavigationActivityList.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Chip, Divider, List, ListItem, ListItemIcon } from "@material-ui/core";
 import DirectionsBikeIcon from "@material-ui/icons/DirectionsBike";
 import DirectionsWalkIcon from "@material-ui/icons/DirectionsWalk";
+import { ActivityMode } from "../../utils/Enums";
 
 export default function NavigationActivityList({ countedActivities, handleNavigationClick }) {
     const activities = [...countedActivities.keys()].sort();
@@ -12,7 +13,7 @@ export default function NavigationActivityList({ countedActivities, handleNaviga
                 {activities.map((a) => (
                     <ListItem key={a} button onClick={() => handleNavigationClick(`/tracks?activity=${a}`)}>
                         <ListItemIcon>
-                            {a === 0 ? <DirectionsBikeIcon /> : <DirectionsWalkIcon />}
+                            {a === ActivityMode.BIKE ? <DirectionsBikeIcon /> : <DirectionsWalkIcon />}
                         </ListItemIcon>
                         <Chip size="small" label={countedActivities.get(a)} />
                     </ListItem>
