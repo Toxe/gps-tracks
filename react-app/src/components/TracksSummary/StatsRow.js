349a3f3970aa1a493e7cb263e9723b25b3ff430f
Toxe <toxe@toxe.net>
2020-08-21 12:29:18 +0200

    Change summary format to table

--- react-app/src/components/TracksSummary/StatsRow.js
+++ react-app/src/components/TracksSummary/StatsRow.js
@@ -1,20 +1,34 @@
 import React from "react";
-import { Box, Chip } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
 import ActivityIcon from "../Track/ActivityIcon";
 import Distance from "../Track/Distance";
 import Duration from "../Track/Duration";
 
+const useStyles = makeStyles((theme) => ({
+    smallGap: {
+        paddingLeft: theme.spacing(1),
+    },
+    wideGap: {
+        paddingLeft: theme.spacing(4),
+    },
+    rightAlign: {
+        textAlign: "right",
+    },
+}));
+
 export default function StatsRow({ stats }) {
-    if (!stats) {
+    const classes = useStyles();
+
+    if (!stats)
         return null;
-    }
 
     return (
-        <Box display="flex">
-            <Chip size="small" label={stats.count} />
-            <ActivityIcon activity={stats.activity} />
-            <Distance distance={stats.distance} />
-            <Duration duration={stats.duration} />
-        </Box>
+        <tr>
+            <td><ActivityIcon activity={stats.activity} /></td>
+            <td className={`${classes.rightAlign} ${classes.smallGap}`}><Typography variant="body1">{stats.count} Tracks</Typography></td>
+            <td className={`${classes.rightAlign} ${classes.wideGap}`}><Distance distance={stats.distance} /></td>
+            <td className={classes.wideGap}><Duration duration={stats.duration} /></td>
+        </tr>
     );
 }
--- react-app/src/components/TracksSummary/TracksSummary.js
+++ react-app/src/components/TracksSummary/TracksSummary.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
-import StatsRow from "./StatsRow";
+import { makeStyles } from "@material-ui/core/styles";
 import { ActivityMode } from "../../utils/Enums";
+import StatsRow from "./StatsRow";
+
+const useStyles = makeStyles(() => ({
+    table: {
+        "& td": {},
+    },
+}));
 
 function generateStats(allTracks, activity) {
-    if (!allTracks || allTracks.length === 0) {
+    if (!allTracks || allTracks.length === 0)
         return null;
-    }
 
     const tracks = allTracks.filter((t) => t.activity_mode === activity);
     const distance = tracks.reduce((sum, t) => sum + t.length3d, 0.0);
@@ -16,6 +22,7 @@ function generateStats(allTracks, activity) {
 }
 
 export default function TracksSummary({ tracks }) {
+    const classes = useStyles();
     const [stats, setStats] = useState(null);
 
     useEffect(() => {
@@ -27,14 +34,17 @@ export default function TracksSummary({ tracks }) {
         );
     }, [tracks]);
 
-    if (!stats) {
+    if (!stats)
         return null;
-    }
 
     return (
         <div>
-            <StatsRow stats={stats.get(ActivityMode.BIKE)} />
-            <StatsRow stats={stats.get(ActivityMode.HIKING)} />
+            <table className={classes.table}>
+                <tbody>
+                    <StatsRow stats={stats.get(ActivityMode.BIKE)} />
+                    <StatsRow stats={stats.get(ActivityMode.HIKING)} />
+                </tbody>
+            </table>
         </div>
     );
 }
