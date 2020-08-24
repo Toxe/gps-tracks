3564d158cdf22f8ef656f6bcdfc41134876595d8
Toxe <toxe@toxe.net>
2020-08-24 19:11:48 +0200

    Add Leaflet map to single track page

--- react-app/src/content/SingleTrack.js
+++ react-app/src/content/SingleTrack.js
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
 import { useParams } from "react-router-dom";
 import Track from "../components/Track/Track";
 import { useTracks } from "../api/TracksProvider";
+import { Map, Polyline, TileLayer } from "react-leaflet";
+import { latLngBounds } from "leaflet";
+
+const useStyles = makeStyles(() => ({
+    map: {
+        height: 600,
+        width: "100%",
+    },
+}));
 
 export default function SingleTrack() {
+    const classes = useStyles();
     const { trackId } = useParams();
     const { getTrack } = useTracks();
-    const track = getTrack(trackId);
+    const [track, setTrack] = useState(null);
+    const [segments, setSegments] = useState(null);
+    const [bounds, setBounds] = useState(null);
 
-    if (!track)
+    useEffect(() => {
+        async function loadSegments(track) {
+            const response = await axios.get(track.links.segments);
+            setSegments(response.data);
+            setBounds(latLngBounds(response.data));
+        }
+
+        if (track) {
+            loadSegments(track);
+        }
+    }, [track]);
+
+    useEffect(() => {
+        setTrack(getTrack(trackId));
+    }, [trackId, getTrack]);
+
+    if (!bounds) {
         return null;
+    }
 
-    return <Track track={track} />;
+    return (
+        <div>
+            <Track track={track} />
+            <Map bounds={bounds} className={classes.map}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Polyline color="blue" positions={segments} weight={4} />
+            </Map>
+        </div>
+    );
 }
