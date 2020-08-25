fb10a2e0ce1634fc2edac58dfffd5b489364d389
Toxe <toxe@toxe.net>
2020-08-25 22:35:31 +0200

    Can click on thumbnail to open track page

--- react-app/src/components/Track/Thumbnail.js
+++ react-app/src/components/Track/Thumbnail.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Link } from "@material-ui/core";
 
 export default function Thumbnail({ track }) {
-    return <img src={track.links.thumbnail} width={128} height={128} alt="track thumbnail" />;
+    return (
+        <Link component={RouterLink} to={`/tracks/${track.id}`}>
+            <img src={track.links.thumbnail} width={128} height={128} alt="track thumbnail" />
+        </Link>
+    );
 }
