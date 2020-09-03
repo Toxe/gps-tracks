05656a806f85d805e27a0ff0a36d7efe017d353a
Toxe <toxe@toxe.net>
2020-09-03 18:07:34 +0200

    Make code easier to read

--- rest-api/app/__init__.py
+++ rest-api/app/__init__.py
@@ -24,15 +24,13 @@ def create_app(config_class=Config):
     jwt.init_app(app)
 
     from app.errors import bp as errors_bp
-    app.register_blueprint(errors_bp)
-
     from app.api import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix="/api")
-
     from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-
     from app.thumbnails import bp as thumbnails_bp
+
+    app.register_blueprint(errors_bp)
+    app.register_blueprint(api_bp, url_prefix="/api")
+    app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(thumbnails_bp, url_prefix="/thumbnails")
 
     return app
