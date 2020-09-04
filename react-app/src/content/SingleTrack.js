29b9d4fe5d564728a07498f8e435568d31eaeeca
Toxe <toxe@toxe.net>
2020-09-04 16:39:18 +0200

    Add download and delete buttons

--- react-app/src/content/SingleTrack.js
+++ react-app/src/content/SingleTrack.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useTranslation } from "react-i18next";
 import { makeStyles } from "@material-ui/core/styles";
 import { useParams } from "react-router-dom";
+import { Box, Button } from "@material-ui/core";
 import Track from "../components/Track/Track";
 import { useTracks } from "../api/TracksProvider";
 import { Map, Polyline, TileLayer } from "react-leaflet";
 import { latLngBounds } from "leaflet";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     map: {
         height: 600,
         width: "100%",
     },
+    button: {
+        marginLeft: theme.spacing(2),
+    },
 }));
 
 export default function SingleTrack() {
+    const { t } = useTranslation();
     const classes = useStyles();
     const { trackId } = useParams();
     const { getTrack } = useTracks();
@@ -45,6 +53,14 @@ export default function SingleTrack() {
     return (
         <div>
             <Track track={track} />
+            <Box mb={4} display="flex" justifyContent="flex-end">
+                <Button variant="contained" color="primary" startIcon={<SaveIcon />} className={classes.button}>
+                    {t("button_download_track")}
+                </Button>
+                <Button variant="contained" color="secondary" startIcon={<DeleteIcon />} className={classes.button}>
+                    {t("button_delete_track")}
+                </Button>
+            </Box>
             <Map bounds={bounds} className={classes.map}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
