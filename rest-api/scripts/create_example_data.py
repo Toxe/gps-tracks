ef4d8cf2e8ceb2105ff55310e7454093a0544606
Toxe <toxe@toxe.net>
2020-09-10 18:44:56 +0200

    Rename global variable

--- rest-api/scripts/create_example_data.py
+++ rest-api/scripts/create_example_data.py
@@ -1,12 +1,12 @@
 import requests
 
 
-host = "http://localhost:5000"
+HOST = "http://localhost:5000"
 
 
 def post(route, data):
     print("POST", route)
-    r = requests.post("{}{}".format(host, route), json=data)
+    r = requests.post("{}{}".format(HOST, route), json=data)
     if r.status_code < 200 or r.status_code >= 300:
         raise Exception("Request error: %d" % r.status_code)
 
@@ -14,14 +14,14 @@ def post(route, data):
 def upload(route, filename, access_token):
     print("POST {} [{}]".format(route, filename))
     headers = {"Authorization": "Bearer {}".format(access_token)}
-    r = requests.post("{}{}".format(host, route), files={"file": open(filename)}, headers=headers)
+    r = requests.post("{}{}".format(HOST, route), files={"file": open(filename)}, headers=headers)
     if r.status_code < 200 or r.status_code >= 300:
         raise Exception("Request error: %d" % r.status_code)
 
 
 def login(route, email, password):
     print("POST {} [{}]".format(route, email))
-    r = requests.post("{}{}".format(host, route), json={"email": email, "password": password})
+    r = requests.post("{}{}".format(HOST, route), json={"email": email, "password": password})
     if r.status_code < 200 or r.status_code >= 300:
         raise Exception("Request error: %d" % r.status_code)
     data = r.json()
