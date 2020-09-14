029b4f549a1f6643b3e6e22d32d475f98320a533
Toxe <toxe@toxe.net>
2020-09-14 19:53:31 +0200

    Load english translations for tests

--- react-app/src/i18n-tests.js
+++ react-app/src/i18n-tests.js
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import translation from "../public/locales/en/translation.json";
 
 i18n.use(initReactI18next).init({
     keySeparator: false,
@@ -8,7 +9,7 @@ i18n.use(initReactI18next).init({
     },
     lng: "en",
     resources: {
-        en: {},
+        en: { translation },
         de: {},
     },
 });
