e116bd2954ee09e91de7f77020d208e81f7bfd57
Toxe <toxe@toxe.net>
2020-09-30 13:30:56 +0200

    Restructure and improve Duration tests

--- react-app/src/components/Track/__tests__/Duration.js
+++ react-app/src/components/Track/__tests__/Duration.js
@@ -6,82 +6,102 @@ import "jest-extended";
 import "expect-more-jest";
 import Duration from "../Duration";
 
-it("shows negative durations as 00:00", () => {
-    const { getByText } = render(<Duration duration={-1234.0} />);
-    getByText("00:00");
-});
+describe("Duration", () => {
+    describe("With durations from 0 to 59 seconds", () => {
+        test("When duration is 0 seconds, shows 0 minutes", () => {
+            const { getByText } = render(<Duration duration={0.0} />);
+            getByText("00:00");
+        });
 
-it("works with 0 seconds", () => {
-    const { getByText } = render(<Duration duration={0.0} />);
-    getByText("00:00");
-});
+        test("When duration is 1 second, shows 0 minutes", () => {
+            const { getByText } = render(<Duration duration={1.0} />);
+            getByText("00:00");
+        });
 
-it("works with 1 second", () => {
-    const { getByText } = render(<Duration duration={1.0} />);
-    getByText("00:00");
-});
+        test("When duration is 29 seconds, shows 0 minutes", () => {
+            const { getByText } = render(<Duration duration={29.0} />);
+            getByText("00:00");
+        });
 
-it("works with 29 seconds", () => {
-    const { getByText } = render(<Duration duration={29.0} />);
-    getByText("00:00");
-});
+        test("When duration is 30 seconds, shows 1 minute", () => {
+            const { getByText } = render(<Duration duration={30.0} />);
+            getByText("00:01");
+        });
 
-it("works with 30 seconds", () => {
-    const { getByText } = render(<Duration duration={30.0} />);
-    getByText("00:01");
-});
+        test("When duration is 59 seconds, shows 1 minute", () => {
+            const { getByText } = render(<Duration duration={59.0} />);
+            getByText("00:01");
+        });
+    });
 
-it("works with 59 seconds", () => {
-    const { getByText } = render(<Duration duration={59.0} />);
-    getByText("00:01");
-});
+    describe("With durations from 1 to 59 minutes", () => {
+        test("When duration is 60 seconds, shows 1 minute", () => {
+            const { getByText } = render(<Duration duration={60.0} />);
+            getByText("00:01");
+        });
 
-it("works with 1 minute", () => {
-    const { getByText } = render(<Duration duration={60.0} />);
-    getByText("00:01");
-});
+        test("When duration is 1 minute and 1 second, shows 1 minute", () => {
+            const { getByText } = render(<Duration duration={61.0} />);
+            getByText("00:01");
+        });
 
-it("works with 1 minute and 1 second", () => {
-    const { getByText } = render(<Duration duration={61.0} />);
-    getByText("00:01");
-});
+        test("When duration is 59 minutes, shows 59 minutes", () => {
+            const { getByText } = render(<Duration duration={59.0 * 60.0} />);
+            getByText("00:59");
+        });
+    });
 
-it("works with 59 minutes", () => {
-    const { getByText } = render(<Duration duration={59.0 * 60.0} />);
-    getByText("00:59");
-});
+    describe("With durations of 1 hour and upwards", () => {
+        test("When duration is 1 hour, shows 1 hour", () => {
+            const { getByText } = render(<Duration duration={3600.0} />);
+            getByText("01:00");
+        });
 
-it("works with 1 hour", () => {
-    const { getByText } = render(<Duration duration={3600.0} />);
-    getByText("01:00");
-});
+        test("When duration is 1 hour and 1 minute, shows 01:01", () => {
+            const { getByText } = render(<Duration duration={3600.0 + 60.0} />);
+            getByText("01:01");
+        });
 
-it("works with 1 hour and 1 minute", () => {
-    const { getByText } = render(<Duration duration={3660.0} />);
-    getByText("01:01");
-});
+        test("When duration is 12 hours, shows 12:00", () => {
+            const { getByText } = render(<Duration duration={12.0 * 3600.0} />);
+            getByText("12:00");
+        });
 
-it("works with 12 hours", () => {
-    const { getByText } = render(<Duration duration={12.0 * 3600.0} />);
-    getByText("12:00");
-});
+        test("When duration is 24 hours, shows 24:00", () => {
+            const { getByText } = render(<Duration duration={24.0 * 3600.0} />);
+            getByText("24:00");
+        });
 
-it("works with 24 hours", () => {
-    const { getByText } = render(<Duration duration={24.0 * 3600.0} />);
-    getByText("24:00");
-});
+        test("When duration is 48 hours, shows 48:00", () => {
+            const { getByText } = render(<Duration duration={48.0 * 3600.0} />);
+            getByText("48:00");
+        });
 
-it("works with 24 hours", () => {
-    const { getByText } = render(<Duration duration={24.0 * 3600.0} />);
-    getByText("24:00");
-});
+        test("When duration is 250 hours, shows 250:00", () => {
+            const { getByText } = render(<Duration duration={250.0 * 3600.0} />);
+            getByText("250:00");
+        });
+    });
 
-it("works with 48 hours", () => {
-    const { getByText } = render(<Duration duration={48.0 * 3600.0} />);
-    getByText("48:00");
-});
+    describe("With faulty data", () => {
+        test("When duration is negative, shows 00:00", () => {
+            const { getByText } = render(<Duration duration={-1234.0} />);
+            getByText("00:00");
+        });
 
-it("works with 250 hours", () => {
-    const { getByText } = render(<Duration duration={250.0 * 3600.0} />);
-    getByText("250:00");
+        test("When duration is null, shows 00:00", () => {
+            const { getByText } = render(<Duration duration={null} />);
+            getByText("00:00");
+        });
+
+        test("When duration is undefined, shows 00:00", () => {
+            const { getByText } = render(<Duration duration={undefined} />);
+            getByText("00:00");
+        });
+
+        test("When no duration given, shows 00:00", () => {
+            const { getByText } = render(<Duration />);
+            getByText("00:00");
+        });
+    });
 });
