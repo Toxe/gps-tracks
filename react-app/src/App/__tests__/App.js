d9f094be33e6327fd55742a80da2e278a99a4096
Toxe <toxe@toxe.net>
2020-09-30 15:03:39 +0200

    Move Un/AuthenticatedApp tests into one file

---
+++ react-app/src/App/__tests__/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../../i18n-tests";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import "jest-extended";
+import "expect-more-jest";
+import jwt from "jsonwebtoken";
+import { AuthProvider } from "../../Auth/AuthProvider";
+import App from "../App";
+
+describe("App", () => {
+    describe("With authenticated user", () => {
+        test("When user is logged in, show AuthenticatedApp", () => {
+            localStorage.setItem("access_token", jwt.sign({ identity: 1 }, "secret"));
+            localStorage.setItem("refresh_token", jwt.sign({ identity: 1 }, "secret"));
+
+            const { getByRole } = render(
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            );
+
+            getByRole("button", { name: "Upload" });
+        });
+    });
+
+    describe("With no authenticated user", () => {
+        test("When user is not logged in, show UnauthenticatedApp", () => {
+            localStorage.removeItem("access_token");
+            localStorage.removeItem("refresh_token");
+
+            const { getByRole } = render(
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            );
+
+            getByRole("button", { name: "Sign in" });
+        });
+    });
+});
