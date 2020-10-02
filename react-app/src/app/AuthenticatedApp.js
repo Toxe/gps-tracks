25a859e8fdfeeb9d7ad6479f942bc230a8537559
Toxe <toxe@toxe.net>
2020-10-02 15:42:20 +0200

    Move TracksProvider up to AuthenticatedApp

--- react-app/src/app/AuthenticatedApp.js
+++ react-app/src/app/AuthenticatedApp.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { UserProvider } from "../api/UserProvider";
+import { TracksProvider } from "../api/TracksProvider";
 import MainPage from "../pages/MainPage";
 import "../Leaflet";
 
 export default function AuthenticatedApp() {
     return (
         <UserProvider>
-            <Routes>
-                <Route path="/*" element={<MainPage />} />
-            </Routes>
+            <TracksProvider>
+                <Routes>
+                    <Route path="/*" element={<MainPage />} />
+                </Routes>
+            </TracksProvider>
         </UserProvider>
     );
 }
--- react-app/src/pages/MainPage.js
+++ react-app/src/pages/MainPage.js
@@ -7,7 +7,6 @@ import AllTracks from "../content/AllTracks";
 import SingleTrack from "../content/SingleTrack";
 import EditTrack from "../content/EditTrack";
 import AuthInfo from "../auth/AuthInfo";
-import { TracksProvider } from "../api/TracksProvider";
 import { TracksFilterProvider } from "../components/TracksFilter/TracksFilterProvider";
 import { TracksSortProvider } from "../components/TracksSort/TracksSortProvider";
 import { TracksSearchProvider } from "../components/TracksSearch/TracksSearchProvider";
@@ -21,29 +20,27 @@ export default function MainPage() {
     };
 
     return (
-        <TracksProvider>
-            <TracksFilterProvider>
-                <TracksSearchProvider>
-                    <TracksSortProvider>
-                        <Header handleMobileNavigationToggle={handleMobileNavigationToggle} />
-                        <Navigation
-                            mobileNavigationOpen={mobileNavigationOpen}
-                            handleMobileNavigationToggle={handleMobileNavigationToggle}
-                        />
-                        <PageContent>
-                            <LastVisitedAllTracksPageProvider>
-                                <Routes>
-                                    <Route path="tracks" element={<AllTracks />} />
-                                    <Route path="tracks/:trackId" element={<SingleTrack />} />
-                                    <Route path="tracks/:trackId/edit" element={<EditTrack />} />
-                                    <Route path="*" element={<Navigate to="/tracks" replace />} />
-                                </Routes>
-                            </LastVisitedAllTracksPageProvider>
-                            {Boolean(parseInt(process.env.REACT_APP_AUTH_INFO)) && <AuthInfo />}
-                        </PageContent>
-                    </TracksSortProvider>
-                </TracksSearchProvider>
-            </TracksFilterProvider>
-        </TracksProvider>
+        <TracksFilterProvider>
+            <TracksSearchProvider>
+                <TracksSortProvider>
+                    <Header handleMobileNavigationToggle={handleMobileNavigationToggle} />
+                    <Navigation
+                        mobileNavigationOpen={mobileNavigationOpen}
+                        handleMobileNavigationToggle={handleMobileNavigationToggle}
+                    />
+                    <PageContent>
+                        <LastVisitedAllTracksPageProvider>
+                            <Routes>
+                                <Route path="tracks" element={<AllTracks />} />
+                                <Route path="tracks/:trackId" element={<SingleTrack />} />
+                                <Route path="tracks/:trackId/edit" element={<EditTrack />} />
+                                <Route path="*" element={<Navigate to="/tracks" replace />} />
+                            </Routes>
+                        </LastVisitedAllTracksPageProvider>
+                        {Boolean(parseInt(process.env.REACT_APP_AUTH_INFO)) && <AuthInfo />}
+                    </PageContent>
+                </TracksSortProvider>
+            </TracksSearchProvider>
+        </TracksFilterProvider>
     );
 }
