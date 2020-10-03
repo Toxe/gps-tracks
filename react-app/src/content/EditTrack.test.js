80e2a938df4672d3cb0c35b9c4959cd356c5526d
Toxe <toxe@toxe.net>
2020-10-03 23:09:01 +0200

    Add tests to EditTrack

--- react-app/src/content/EditTrack.test.js
+++ react-app/src/content/EditTrack.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../i18n-tests";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import "jest-extended";
 import "expect-more-jest";
@@ -18,6 +19,21 @@ function matchByTextContent(queryText) {
     return (content, node) => node.textContent === queryText;
 }
 
+function setupEditTrackPage() {
+    const { access_token, refresh_token } = sampleAuthTokens(1);
+    saveAuthTokensToLocalStorage(access_token, refresh_token);
+
+    axiosMock.get.mockResolvedValueOnce({ data: sampleUser(1) }).mockResolvedValueOnce({ data: sampleTracks() });
+
+    window.history.pushState({}, "Test Page", "/tracks/21/edit");
+
+    return render(
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    );
+}
+
 describe("EditTrack", () => {
     afterEach(() => {
         removeAuthTokensFromLocalStorage();
@@ -25,25 +41,36 @@ describe("EditTrack", () => {
 
     describe("With existing track", () => {
         test("When loading route /tracks/21/edit, show edit page for track 21", async () => {
-            const { access_token, refresh_token } = sampleAuthTokens(1);
-            saveAuthTokensToLocalStorage(access_token, refresh_token);
-
-            axiosMock.get
-                .mockResolvedValueOnce({ data: sampleUser(1) })
-                .mockResolvedValueOnce({ data: sampleTracks() });
-
-            window.history.pushState({}, "Test Page", "/tracks/21/edit");
-
-            const { findByText } = render(
-                <AuthProvider>
-                    <App />
-                </AuthProvider>
-            );
+            const { findByText } = setupEditTrackPage();
 
             await findByText("Track 21");
             await findByText(matchByTextContent("Edit track “Track 21”"));
             await findByText("Cancel");
             await findByText("Save Changes");
+        });
+
+        test('When selecting different activity mode, "Save Changes" button becomes enabled', async () => {
+            const { findByRole } = setupEditTrackPage();
+
+            const saveChangesButton = await findByRole("button", { name: "Save Changes" });
+            const hikingRadioButton = await findByRole("radio", { name: "Hiking" });
+
+            expect(saveChangesButton).toBeDisabled();
+
+            userEvent.click(hikingRadioButton);
+            expect(saveChangesButton).toBeEnabled();
+        });
+
+        test('When typing in a new track title, "Save Changes" button becomes enabled', async () => {
+            const { findByRole, findByDisplayValue } = setupEditTrackPage();
+
+            const saveChangesButton = await findByRole("button", { name: "Save Changes" });
+            const titleTextbox = await findByDisplayValue("Track 21");
+
+            expect(saveChangesButton).toBeDisabled();
+
+            userEvent.type(titleTextbox, "new title");
+            expect(saveChangesButton).toBeEnabled();
         });
     });
 
