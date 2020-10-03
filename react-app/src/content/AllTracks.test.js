77b451a0c6b6e193796f3bb20788cefbb283861f
Toxe <toxe@toxe.net>
2020-10-03 12:49:05 +0200

    Add AllTracks test

---
+++ react-app/src/content/AllTracks.test.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "../i18n-tests";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import "jest-extended";
+import "expect-more-jest";
+import axiosMock from "axios";
+import { sampleAuthTokens } from "../test/sampleAuthTokens";
+import { AuthProvider } from "../auth/AuthProvider";
+import { saveAuthTokensToLocalStorage, removeAuthTokensFromLocalStorage } from "../auth/API";
+import { sampleTracks } from "../test/sampleTracks";
+import { sampleUser } from "../test/sampleUsers";
+import App from "../app/App";
+
+jest.mock("axios");
+
+describe("AllTracks", () => {
+    afterEach(() => {
+        removeAuthTokensFromLocalStorage();
+    });
+
+    describe("With existing tracks", () => {
+        test("Should show list of tracks", async () => {
+            const { access_token, refresh_token } = sampleAuthTokens(1);
+            saveAuthTokensToLocalStorage(access_token, refresh_token);
+
+            axiosMock.get
+                .mockResolvedValueOnce({ data: sampleUser(1) })
+                .mockResolvedValueOnce({ data: sampleTracks() });
+
+            const { findByText } = render(
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            );
+
+            await findByText("Track 025");
+        });
+    });
+
+    describe("Without tracks", () => {
+        test('Should show "No tracks found" text', async () => {
+            const { access_token, refresh_token } = sampleAuthTokens(1);
+            saveAuthTokensToLocalStorage(access_token, refresh_token);
+
+            axiosMock.get.mockResolvedValueOnce({ data: sampleUser(1) }).mockResolvedValueOnce({ data: [] });
+
+            const { findByText } = render(
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            );
+
+            await findByText("No tracks found.");
+        });
+    });
+});
