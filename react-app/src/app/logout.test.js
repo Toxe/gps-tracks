aaa45b42587b17281051084940bf02aabc583bcc
Toxe <toxe@toxe.net>
2020-10-05 21:10:59 +0200

    Add logout test

---
+++ react-app/src/app/logout.test.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "../i18n-tests";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import "jest-extended";
+import "expect-more-jest";
+import axiosMock from "axios";
+import { sampleAuthTokens } from "../test/sampleAuthTokens";
+import { sampleTracks } from "../test/sampleTracks";
+import { sampleUser } from "../test/sampleUsers";
+import { AuthProvider } from "../auth/AuthProvider";
+import {
+    saveAuthTokensToLocalStorage,
+    removeAuthTokensFromLocalStorage,
+    getAuthTokensFromLocalStorage,
+} from "../auth/API";
+import App from "./App";
+
+jest.mock("axios");
+
+describe("Logout from the application", () => {
+    afterEach(() => {
+        axiosMock.get.mockReset();
+        removeAuthTokensFromLocalStorage();
+    });
+
+    describe("With authenticated user", () => {
+        test('When click on "Logout" button, logout and navigate to login page', async () => {
+            const { access_token, refresh_token } = sampleAuthTokens(1);
+            saveAuthTokensToLocalStorage(access_token, refresh_token);
+
+            axiosMock.get
+                .mockResolvedValueOnce({ data: sampleUser(1) })
+                .mockResolvedValueOnce({ data: sampleTracks() });
+
+            axiosMock.delete.mockResolvedValueOnce({}).mockResolvedValueOnce({});
+
+            const { findByRole, findByText } = render(
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            );
+
+            // open user menu and click on "Logout"
+            userEvent.click(await findByRole("button", { name: "User #1" }));
+            userEvent.click(await findByText("Logout"));
+
+            // wait for Login page
+            await findByRole("heading", { name: "Sign in" });
+
+            // access and refresh tokens should have been removed from local storage
+            expect(getAuthTokensFromLocalStorage()).toEqual({ access_token: null, refresh_token: null });
+        });
+    });
+});
