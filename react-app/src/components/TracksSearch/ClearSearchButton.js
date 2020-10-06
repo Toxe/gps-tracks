fd7651592895a3aee8df9beba505a47ee5c4ccc8
Toxe <toxe@toxe.net>
2020-10-06 18:22:21 +0200

    Add tooltip to the clear search button

--- react-app/src/components/TracksSearch/ClearSearchButton.js
+++ react-app/src/components/TracksSearch/ClearSearchButton.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { makeStyles } from "@material-ui/core/styles";
-import { IconButton } from "@material-ui/core";
+import { IconButton, Tooltip } from "@material-ui/core";
 import ClearIcon from "@material-ui/icons/Clear";
 
 const useStyles = makeStyles(() => ({
@@ -12,11 +13,20 @@ const useStyles = makeStyles(() => ({
 }));
 
 export default function ClearSearchButton({ disabled, handleClick }) {
+    const { t } = useTranslation();
     const classes = useStyles();
 
-    return (
-        <IconButton disabled={disabled} disableRipple className={classes.button} onClick={handleClick}>
-            <ClearIcon color={disabled ? "disabled" : "action"} fontSize="small" />
-        </IconButton>
-    );
+    const createButton = () => {
+        return (
+            <IconButton disabled={disabled} disableRipple className={classes.button} onClick={handleClick}>
+                <ClearIcon color={disabled ? "disabled" : "action"} fontSize="small" />
+            </IconButton>
+        );
+    };
+
+    if (disabled) {
+        return createButton();
+    } else {
+        return <Tooltip title={t("search_button_clear_search")}>{createButton()}</Tooltip>;
+    }
 }
