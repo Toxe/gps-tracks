a0f6851465d5efe0910baaf6ba0a1ca53d7f3f51
Toxe <toxe@toxe.net>
2020-10-06 14:53:18 +0200

    Add LanguageSelection test

---
+++ react-app/src/components/Header/LanguageSelection.test.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "../../i18n-tests";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import "jest-extended";
+import "expect-more-jest";
+import axiosMock from "axios";
+import { sampleAuthTokens } from "../../test/sampleAuthTokens";
+import { sampleTracks } from "../../test/sampleTracks";
+import { sampleUser } from "../../test/sampleUsers";
+import { AuthProvider } from "../../auth/AuthProvider";
+import { saveAuthTokensToLocalStorage, removeAuthTokensFromLocalStorage } from "../../auth/API";
+import App from "../../app/App";
+
+jest.mock("axios");
+
+describe("LanguageSelection", () => {
+    afterEach(() => {
+        axiosMock.get.mockReset();
+        removeAuthTokensFromLocalStorage();
+    });
+
+    describe('With language set to "en"', () => {
+        test('When click on "Deutsch", change language to German', async () => {
+            const { access_token, refresh_token } = sampleAuthTokens(1);
+            saveAuthTokensToLocalStorage(access_token, refresh_token);
+
+            axiosMock.get
+                .mockResolvedValueOnce({ data: sampleUser(1) })
+                .mockResolvedValueOnce({ data: sampleTracks() });
+
+            const { findByRole, findByLabelText } = render(
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            );
+
+            // open language selection menu and click on "Deutsch"
+            userEvent.click(await findByLabelText("English"));
+            userEvent.click(await findByLabelText("Deutsch"));
+
+            // the "Upload" button should now be called "Hochladen"
+            await findByRole("button", { name: "Hochladen" });
+        });
+    });
+});
--- react-app/src/i18n-tests.js
+++ react-app/src/i18n-tests.js
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import translation from "../public/locales/en/translation.json";
+import translation_en from "../public/locales/en/translation.json";
+import translation_de from "../public/locales/de/translation.json";
 
 i18n.use(initReactI18next).init({
     keySeparator: false,
@@ -9,8 +10,8 @@ i18n.use(initReactI18next).init({
     },
     lng: "en",
     resources: {
-        en: { translation },
-        de: {},
+        en: { translation: translation_en },
+        de: { translation: translation_de },
     },
 });
 
