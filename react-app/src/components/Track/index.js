4ca8eed4a04dd9145ca5d003c2319aaa213c8e4a
Toxe <toxe@toxe.net>
2020-10-07 18:26:45 +0200

    Move TrackMap and TrackDetails to SingleTrack

--- react-app/src/components/Track/index.js
+++ react-app/src/components/Track/index.js
@@ -6,6 +6,5 @@ import FormattedDate from "./FormattedDate";
 import Thumbnail from "./Thumbnail";
 import Title from "./Title";
 import Track from "./Track";
-import TrackDetails from "./TrackDetails";
 
-export { ActivityIcon, AverageSpeed, Distance, Duration, FormattedDate, Thumbnail, Title, Track, TrackDetails };
+export { ActivityIcon, AverageSpeed, Distance, Duration, FormattedDate, Thumbnail, Title, Track };
