073f39719611afb68be35c350bdb3bbebeb672ef
Toxe <toxe@toxe.net>
2020-10-09 18:55:28 +0200

    Add sampleGPXFiles to test helper index.js

--- react-app/src/test/index.js
+++ react-app/src/test/index.js
@@ -1,5 +1,6 @@
 export * from "./matchByTextContent";
 export * from "./sampleAuthTokens";
+export * from "./sampleGPXFiles";
 export * from "./sampleTracks";
 export * from "./sampleTrackSegments";
 export * from "./sampleUsers";
