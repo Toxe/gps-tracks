d297daae37123f701f648ba766116569645c41a9
Toxe <toxe@toxe.net>
2020-10-15 21:21:19 +0200

    Simplify test setup

--- react-app/src/App/AuthenticatedApp/MainPage/EditTrack/hooks/useEditTrack.test.js
+++ react-app/src/App/AuthenticatedApp/MainPage/EditTrack/hooks/useEditTrack.test.js
@@ -15,6 +15,14 @@ jest.mock("react-router-dom", () => ({
 
 jest.mock("../../../TracksProvider");
 
+function renderUseEditTrackHook(track, updateTrack) {
+    const getTrack = jest.fn(() => track);
+    useTracks.mockReturnValue({ getTrack, updateTrack });
+
+    const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
+    return renderHook(() => useEditTrack(), { wrapper });
+}
+
 afterEach(() => {
     jest.resetAllMocks();
     jest.restoreAllMocks();
@@ -24,22 +32,14 @@ describe("useEditTrack()", () => {
     describe("Default values", () => {
         test('"track" should be the current track', () => {
             const track = sampleTrack(21);
-            const getTrack = jest.fn(() => track);
-            useTracks.mockReturnValue({ getTrack });
-
-            const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useEditTrack(), { wrapper });
+            const { result } = renderUseEditTrackHook(track, null);
 
             expect(result.current.track).toBe(track);
         });
 
         test('"requestError" should be null', () => {
             const track = sampleTrack(21);
-            const getTrack = jest.fn(() => track);
-            useTracks.mockReturnValue({ getTrack });
-
-            const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useEditTrack(), { wrapper });
+            const { result } = renderUseEditTrackHook(track, null);
 
             expect(result.current.requestError).toBeNull();
         });
@@ -47,13 +47,9 @@ describe("useEditTrack()", () => {
 
     describe("Saving changes", () => {
         test("When saving changes, should not show a request error", async () => {
-            const track = sampleTrack(21);
-            const getTrack = jest.fn(() => track);
             const updateTrack = jest.fn();
-            useTracks.mockReturnValue({ getTrack, updateTrack });
-
-            const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useEditTrack(), { wrapper });
+            const track = sampleTrack(21);
+            const { result } = renderUseEditTrackHook(track, updateTrack);
 
             await act(() => result.current.handleSave({ title: "new title", activity_mode: 0 }));
 
@@ -63,15 +59,10 @@ describe("useEditTrack()", () => {
 
         test("When an exception is raised, should show request error", async () => {
             const track = sampleTrack(21);
-            const getTrack = jest.fn(() => track);
             const updateTrack = jest.fn(() => {
                 throw new Error("something went wrong");
             });
-
-            useTracks.mockReturnValue({ getTrack, updateTrack });
-
-            const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useEditTrack(), { wrapper });
+            const { result } = renderUseEditTrackHook(track, updateTrack);
 
             await act(() => result.current.handleSave({ title: "new title", activity_mode: 0 }));
 
@@ -83,11 +74,7 @@ describe("useEditTrack()", () => {
     describe("Cancel changes", () => {
         test("When canceling changes, should navigate to single track page", () => {
             const track = sampleTrack(21);
-            const getTrack = jest.fn(() => track);
-            useTracks.mockReturnValue({ getTrack });
-
-            const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useEditTrack(), { wrapper });
+            const { result } = renderUseEditTrackHook(track, null);
 
             act(() => result.current.handleCancel());
         });
