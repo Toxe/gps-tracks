be440d991c9f6cd1f5b302a373819d1c9f7bc759
Toxe <toxe@toxe.net>
2020-10-16 14:54:13 +0200

    Add Pager for the tracks list

---
+++ react-app/src/App/AuthenticatedApp/MainPage/AllTracks/List/Pager.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Pagination } from "@material-ui/lab";
+
+const useStyles = makeStyles((theme) => ({
+    pager: {
+        marginBottom: theme.spacing(2),
+    },
+}));
+
+export default function Pager({ showPager, numPages, page, handleChangePage }) {
+    const classes = useStyles();
+
+    if (!showPager || numPages === 0 || page < 1) {
+        return null;
+    }
+
+    return <Pagination count={numPages} page={page} onChange={handleChangePage} className={classes.pager} />;
+}
---
+++ react-app/src/App/AuthenticatedApp/MainPage/AllTracks/List/Pager.test.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import "jest-extended";
+import "expect-more-jest";
+import Pager from "./Pager";
+
+describe("Pager", () => {
+    describe("Show Pager", () => {
+        test("With numPages 3, should show pager with 3 pages", async () => {
+            const { getByRole } = render(<Pager showPager={true} numPages={3} page={1} handleChangePage={jest.fn()} />);
+
+            getByRole("button", { name: "page 1" });
+            getByRole("button", { name: "Go to page 2" });
+            getByRole("button", { name: "Go to page 3" });
+            getByRole("button", { name: "Go to next page" });
+            getByRole("button", { name: "Go to previous page" });
+        });
+
+        test("With click on page, should call handleChangePage", async () => {
+            const handleChangePage = jest.fn();
+
+            const { getByRole } = render(
+                <Pager showPager={true} numPages={3} page={1} handleChangePage={handleChangePage} />
+            );
+
+            userEvent.click(getByRole("button", { name: "Go to next page" }));
+
+            expect(handleChangePage).toHaveBeenCalled();
+        });
+    });
+
+    describe("Hide Pager", () => {
+        test("When showPager is false, should not show pager and return null", async () => {
+            const { container } = render(<Pager showPager={false} numPages={5} page={1} handleChangePage={jest.fn()} />);
+            expect(container).toBeEmptyDOMElement();
+        });
+
+        test("When numPages is 0, should not show pager and return null", async () => {
+            const { container } = render(<Pager showPager={true} numPages={0} page={1} handleChangePage={jest.fn()} />);
+            expect(container).toBeEmptyDOMElement();
+        });
+
+        test("When page is 0, should not show pager and return null", async () => {
+            const { container } = render(<Pager showPager={true} numPages={5} page={0} handleChangePage={jest.fn()} />);
+            expect(container).toBeEmptyDOMElement();
+        });
+    });
+});
