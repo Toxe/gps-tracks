cf57b5a348664bddb4c69ff6aa465540f8fa6194
Toxe <toxe@toxe.net>
2020-10-22 12:12:43 +0200

    Add useAllTracks test

---
+++ react-app/src/App/AuthenticatedApp/MainPage/AllTracks/hooks/useAllTracks.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { renderHook, act } from "@testing-library/react-hooks";
+import "@testing-library/jest-dom";
+import "jest-extended";
+import "expect-more-jest";
+import { BrowserRouter } from "react-router-dom";
+import { sampleTracks } from "../../../../../test";
+import { useTracks } from "../../../TracksProvider";
+import { MainPageProviders } from "../../MainPageProviders";
+import { useAllTracks } from ".";
+
+jest.mock("../../../TracksProvider");
+
+afterEach(() => {
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+});
+
+describe("useAllTracks()", () => {
+    describe("With tracks", () => {
+        test("When all filters are set to default, should return list of all tracks", () => {
+            const tracks = sampleTracks();
+
+            useTracks.mockReturnValue({ tracks });
+
+            const wrapper = ({ children }) => (
+                <BrowserRouter>
+                    <MainPageProviders>{children}</MainPageProviders>
+                </BrowserRouter>
+            );
+            const { result } = renderHook(() => useAllTracks(), { wrapper });
+
+            expect(result.current.filteredAndSortedTracks).toHaveLength(tracks.length);
+        });
+    });
+});
