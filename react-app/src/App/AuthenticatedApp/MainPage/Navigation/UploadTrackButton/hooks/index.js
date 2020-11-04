381ef6453e5564a35c478798deeecc937136603b
Toxe <toxe@toxe.net>
2020-11-04 19:34:20 +0100

    Move UploadResultsSnackbar state into hook

--- react-app/src/App/AuthenticatedApp/MainPage/Navigation/UploadTrackButton/hooks/index.js
+++ react-app/src/App/AuthenticatedApp/MainPage/Navigation/UploadTrackButton/hooks/index.js
@@ -1,1 +1,2 @@
 export { default as useUploadTrackButton } from "./useUploadTrackButton";
+export { default as useUploadResultsSnackbar } from "./useUploadResultsSnackbar";
---
+++ react-app/src/App/AuthenticatedApp/MainPage/Navigation/UploadTrackButton/hooks/useUploadResultsSnackbar.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+export default function useUploadResultsSnackbar() {
+    const { t } = useTranslation();
+    const [open, setOpen] = useState(true);
+
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    const getMessage = (numFiles, numFilesUploadedSuccessfully) => {
+        if (numFilesUploadedSuccessfully === 0) {
+            // no file imported
+            return ["error", t("upload_error", { count: numFiles })];
+        } else {
+            // at least one file imported
+            if (numFilesUploadedSuccessfully === numFiles && numFiles >= 1) {
+                return ["success", t("upload_success", { count: numFiles })];
+            } else {
+                return ["warning", t("upload_success_partial", { numFilesUploadedSuccessfully, numFiles })];
+            }
+        }
+    };
+
+    return { open, handleClose, getMessage };
+}
