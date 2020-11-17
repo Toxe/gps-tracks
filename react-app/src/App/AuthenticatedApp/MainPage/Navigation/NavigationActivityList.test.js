9783f76a01a092ea1b8be79717c5db9f2a247d2b
Toxe <toxe@toxe.net>
2020-11-17 20:51:42 +0100

    Add NavigationActivityList test

---
+++ react-app/src/App/AuthenticatedApp/MainPage/Navigation/NavigationActivityList.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../../../../i18n-tests";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import "jest-extended";
+import "expect-more-jest";
+import NavigationActivityList from "./NavigationActivityList";
+
+jest.mock("../../TracksProvider");
+
+afterEach(() => {
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+});
+
+describe("NavigationActivityList", () => {
+    describe("With available activities", () => {
+        test("When clicking on a list item, should call handleNavigationClick", async () => {
+            const countedActivities = new Map([
+                [0, 2],
+                [1, 3],
+            ]);
+            const handleNavigationClick = jest.fn();
+
+            const { findByRole } = render(
+                <NavigationActivityList
+                    countedActivities={countedActivities}
+                    handleNavigationClick={handleNavigationClick}
+                />
+            );
+
+            userEvent.click(await findByRole("button", { name: "3" }));
+
+            expect(handleNavigationClick).toHaveBeenCalled();
+        });
+    });
+});
