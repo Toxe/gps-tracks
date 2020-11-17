1e2443195237b4f68ba40ae8ac945fa9d656a265
Toxe <toxe@toxe.net>
2020-11-17 20:51:52 +0100

    Add NavigationYearList test

---
+++ react-app/src/App/AuthenticatedApp/MainPage/Navigation/NavigationYearList.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../../../../i18n-tests";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import "jest-extended";
+import "expect-more-jest";
+import NavigationYearList from "./NavigationYearList";
+
+jest.mock("../../TracksProvider");
+
+afterEach(() => {
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+});
+
+describe("NavigationYearList", () => {
+    describe("With available years", () => {
+        test("When clicking on a list item, should call handleNavigationClick", async () => {
+            const countedYears = new Map([
+                [2019, 2],
+                [2020, 3],
+            ]);
+            const handleNavigationClick = jest.fn();
+
+            const { findByText } = render(
+                <NavigationYearList countedYears={countedYears} handleNavigationClick={handleNavigationClick} />
+            );
+
+            userEvent.click(await findByText("2020"));
+
+            expect(handleNavigationClick).toHaveBeenCalled();
+        });
+    });
+});
