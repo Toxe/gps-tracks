9bebeeb018207ccc4ed6adb7ad4978a6ce927280
Toxe <toxe@toxe.net>
2020-11-30 16:19:05 +0100

    Update useFiltering tests

--- react-app/src/App/AuthenticatedApp/MainPage/AllTracks/Filter/hooks/useFiltering.test.js
+++ react-app/src/App/AuthenticatedApp/MainPage/AllTracks/Filter/hooks/useFiltering.test.js
@@ -15,35 +15,39 @@ afterEach(() => {
 
 describe("useFiltering()", () => {
     describe("With tracks, checking available activities", () => {
-        test('When having only activity 0, should return activities list ["0"]', () => {
+        test('When having only activity 0, listAvailableActivities should return activities list ["0"]', () => {
             const tracks = [
                 { activity_mode: 0, time_start: "2017-06-02T13:18:14.999000" },
                 { activity_mode: 0, time_start: "2018-06-02T13:18:14.999000" },
             ];
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            expect(result.current.availableActivities).toBeArrayOfStrings();
-            expect(result.current.availableActivities).toHaveLength(1);
-            expect(result.current.availableActivities).toEqual(["0"]);
+            const availableActivities = result.current.listAvailableActivities(tracks);
+
+            expect(availableActivities).toBeArrayOfStrings();
+            expect(availableActivities).toHaveLength(1);
+            expect(availableActivities).toEqual(["0"]);
         });
 
-        test('When having only activity 1, should return activities list ["1"]', () => {
+        test('When having only activity 1, listAvailableActivities should return activities list ["1"]', () => {
             const tracks = [
                 { activity_mode: 1, time_start: "2017-06-02T13:18:14.999000" },
                 { activity_mode: 1, time_start: "2018-06-02T13:18:14.999000" },
             ];
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            expect(result.current.availableActivities).toBeArrayOfStrings();
-            expect(result.current.availableActivities).toHaveLength(1);
-            expect(result.current.availableActivities).toEqual(["1"]);
+            const availableActivities = result.current.listAvailableActivities(tracks);
+
+            expect(availableActivities).toBeArrayOfStrings();
+            expect(availableActivities).toHaveLength(1);
+            expect(availableActivities).toEqual(["1"]);
         });
 
-        test('When having activities 0 and 1, should return activities list ["0", "1"]', () => {
+        test('When having activities 0 and 1, listAvailableActivities should return activities list ["0", "1"]', () => {
             const tracks = [
                 { activity_mode: 1, time_start: "2017-06-02T13:18:14.999000" },
                 { activity_mode: 0, time_start: "2018-06-02T13:18:14.999000" },
@@ -52,30 +56,34 @@ describe("useFiltering()", () => {
             ];
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            expect(result.current.availableActivities).toBeArrayOfStrings();
-            expect(result.current.availableActivities).toHaveLength(2);
-            expect(result.current.availableActivities).toContainValues(["0", "1"]);
+            const availableActivities = result.current.listAvailableActivities(tracks);
+
+            expect(availableActivities).toBeArrayOfStrings();
+            expect(availableActivities).toHaveLength(2);
+            expect(availableActivities).toContainValues(["0", "1"]);
         });
     });
 
     describe("With tracks, checking available years", () => {
-        test('When having only year 2016, should return years list ["2016"]', () => {
+        test('When having only year 2016, listAvailableYears should return years list ["2016"]', () => {
             const tracks = [
                 { activity_mode: 0, time_start: "2016-06-02T13:18:14.999000" },
                 { activity_mode: 0, time_start: "2016-08-12T13:18:14.999000" },
             ];
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            expect(result.current.availableYears).toBeArrayOfStrings();
-            expect(result.current.availableYears).toHaveLength(1);
-            expect(result.current.availableYears).toEqual(["2016"]);
+            const availableYears = result.current.listAvailableYears(tracks);
+
+            expect(availableYears).toBeArrayOfStrings();
+            expect(availableYears).toHaveLength(1);
+            expect(availableYears).toEqual(["2016"]);
         });
 
-        test('When having years 2016 and 2017, should return years list ["2016", "2017"]', () => {
+        test('When having years 2016 and 2017, listAvailableYears should return years list ["2016", "2017"]', () => {
             const tracks = [
                 { activity_mode: 1, time_start: "2017-02-02T13:18:14.999000" },
                 { activity_mode: 0, time_start: "2016-03-02T13:18:14.999000" },
@@ -84,11 +92,13 @@ describe("useFiltering()", () => {
             ];
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            expect(result.current.availableYears).toBeArrayOfStrings();
-            expect(result.current.availableYears).toHaveLength(2);
-            expect(result.current.availableYears).toContainValues(["2016", "2017"]);
+            const availableYears = result.current.listAvailableYears(tracks);
+
+            expect(availableYears).toBeArrayOfStrings();
+            expect(availableYears).toHaveLength(2);
+            expect(availableYears).toContainValues(["2016", "2017"]);
         });
     });
 
@@ -103,9 +113,9 @@ describe("useFiltering()", () => {
             spyOnHook(useURLParamYear).mockReturnValue({ yearFilter: "" });
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            const filteredTracks = result.current.filterTracks();
+            const filteredTracks = result.current.filterTracks(tracks);
 
             expect(filteredTracks).toBeArrayOfObjects();
             expect(filteredTracks).toBeSameLengthAs(tracks);
@@ -124,9 +134,9 @@ describe("useFiltering()", () => {
             spyOnHook(useURLParamYear).mockReturnValue({ yearFilter: "all" });
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            const filteredTracks = result.current.filterTracks();
+            const filteredTracks = result.current.filterTracks(tracks);
 
             expect(filteredTracks).toBeArrayOfObjects();
             expect(filteredTracks).toBeSameLengthAs(tracks);
@@ -147,9 +157,9 @@ describe("useFiltering()", () => {
             spyOnHook(useURLParamYear).mockReturnValue({ yearFilter: "" });
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            const filteredTracks = result.current.filterTracks();
+            const filteredTracks = result.current.filterTracks(tracks);
 
             expect(filteredTracks).toBeArrayOfObjects();
             expect(filteredTracks).toHaveLength(2);
@@ -167,9 +177,9 @@ describe("useFiltering()", () => {
             spyOnHook(useURLParamYear).mockReturnValue({ yearFilter: "" });
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            const filteredTracks = result.current.filterTracks();
+            const filteredTracks = result.current.filterTracks(tracks);
 
             expect(filteredTracks).toBeEmptyArray();
         });
@@ -188,9 +198,9 @@ describe("useFiltering()", () => {
             spyOnHook(useURLParamYear).mockReturnValue({ yearFilter: "2020" });
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            const filteredTracks = result.current.filterTracks();
+            const filteredTracks = result.current.filterTracks(tracks);
 
             expect(filteredTracks).toBeArrayOfObjects();
             expect(filteredTracks).toHaveLength(2);
@@ -208,9 +218,9 @@ describe("useFiltering()", () => {
             spyOnHook(useURLParamYear).mockReturnValue({ yearFilter: "2020" });
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(tracks), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            const filteredTracks = result.current.filterTracks();
+            const filteredTracks = result.current.filterTracks(tracks);
 
             expect(filteredTracks).toBeEmptyArray();
         });
@@ -219,41 +229,41 @@ describe("useFiltering()", () => {
     describe("Without tracks, checking filterTracks returns", () => {
         test('When "tracks" is empty array, filterTracks should return empty array', () => {
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering([]), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            expect(result.current.filterTracks()).toBeEmptyArray();
+            expect(result.current.filterTracks([])).toBeEmptyArray();
         });
 
         test('When "tracks" is null, filterTracks should return empty array', () => {
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(null), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            expect(result.current.filterTracks()).toBeEmptyArray();
+            expect(result.current.filterTracks(null)).toBeEmptyArray();
         });
     });
 
     describe("Without tracks, checking available activities and years", () => {
         test('When "tracks" is empty array, available activities and years should be empty arrays', () => {
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering([]), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            expect(result.current.availableActivities).toBeEmptyArray();
-            expect(result.current.availableYears).toBeEmptyArray();
+            expect(result.current.listAvailableActivities([])).toBeEmptyArray();
+            expect(result.current.listAvailableYears([])).toBeEmptyArray();
         });
 
         test('When "tracks" is null, available activities and years should be empty arrays', () => {
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering(null), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
-            expect(result.current.availableActivities).toBeEmptyArray();
-            expect(result.current.availableYears).toBeEmptyArray();
+            expect(result.current.listAvailableActivities(null)).toBeEmptyArray();
+            expect(result.current.listAvailableYears(null)).toBeEmptyArray();
         });
     });
 
     describe("With URL params", () => {
         test('When changing activityFilter, should set "activity" URL param', () => {
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering([]), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
             act(() => result.current.handleChangeActivityFilter("1"));
 
@@ -265,7 +275,7 @@ describe("useFiltering()", () => {
 
         test('When changing yearFilter, should set "year" URL param', () => {
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering([]), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
             act(() => result.current.handleChangeYearFilter("2020"));
 
@@ -280,7 +290,7 @@ describe("useFiltering()", () => {
             window.history.replaceState({}, "", `${window.location.pathname}?${searchParams}`);
 
             const wrapper = ({ children }) => <BrowserRouter>{children}</BrowserRouter>;
-            const { result } = renderHook(() => useFiltering([]), { wrapper });
+            const { result } = renderHook(() => useFiltering(), { wrapper });
 
             expect(result.current.activityFilter).toBe("1");
             expect(result.current.yearFilter).toBe("2020");
