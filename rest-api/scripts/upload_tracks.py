6016d3c7da09218851fece32892dfd982e12b2ab
Toxe <toxe@toxe.net>
2020-12-11 13:15:35 +0100

    Add --host command line argument

--- rest-api/scripts/upload_tracks.py
+++ rest-api/scripts/upload_tracks.py
@@ -4,10 +4,7 @@ import jwt
 import requests
 
 
-host = "http://localhost:5000"
-
-
-def login(route, email, password):
+def login(host, route, email, password):
     json = {"email": email, "password": password}
     url = "{}{}".format(host, route)
     r = requests.post(url, json=json)
@@ -18,7 +15,7 @@ def login(route, email, password):
     return (data["access_token"], user_id)
 
 
-def override_track_activity(track, activity_mode_override, headers, logger):
+def override_track_activity(host, track, activity_mode_override, headers, logger):
     logger(
         "PUT activity_mode {} --> {}".format(
             track["activity_mode"], activity_mode_override
@@ -31,7 +28,7 @@ def override_track_activity(track, activity_mode_override, headers, logger):
         raise Exception("Request error: %d" % r.status_code)
 
 
-def post_file(fp, user_id, headers, activity_mode_override, logger):
+def post_file(host, fp, user_id, headers, activity_mode_override, logger):
     logger("POST")
     url = "{}/api/users/{}/gpxfiles".format(host, user_id)
     r = requests.post(url, files={"file": fp}, headers=headers)
@@ -41,24 +38,27 @@ def post_file(fp, user_id, headers, activity_mode_override, logger):
     if activity_mode_override >= 0:
         for track in tracks:
             if track["activity_mode"] != activity_mode_override:
-                override_track_activity(track, activity_mode_override, headers, logger)
+                override_track_activity(
+                    host, track, activity_mode_override, headers, logger
+                )
 
 
 def upload_file(
-    fp, user_id, headers, activity_mode_override, count_files, current_file
+    host, fp, user_id, headers, activity_mode_override, count_files, current_file
 ):
     def logger(text):
         print("[{}/{}] {}: {}".format(current_file, count_files, fp.name, text))
 
-    post_file(fp, user_id, headers, activity_mode_override, logger)
+    post_file(host, fp, user_id, headers, activity_mode_override, logger)
 
 
-def upload_all_files(filenames, user_id, access_token, activity_mode_override):
+def upload_all_files(host, filenames, user_id, access_token, activity_mode_override):
     headers = {"Authorization": "Bearer {}".format(access_token)}
     current_file = 1
     for filename in filenames:
         with open(filename) as fp:
             upload_file(
+                host,
                 fp,
                 user_id,
                 headers,
@@ -80,6 +80,11 @@ def determine_activity_mode_override(args):
 def eval_args():
     parser = argparse.ArgumentParser(description="Import GPX files.")
     parser.add_argument("filenames", help=".gpx files", nargs="+")
+    parser.add_argument(
+        "--host",
+        help="REST API server host name and port (default: %(default)s)",
+        default="http://localhost:5000",
+    )
     activity_mode_group = parser.add_mutually_exclusive_group()
     activity_mode_group.add_argument(
         "--bike", help="set activity mode to bike", action="store_true"
@@ -89,13 +94,13 @@ def eval_args():
     )
     args = parser.parse_args()
     activity_mode_override = determine_activity_mode_override(args)
-    return (args.filenames, activity_mode_override)
+    return (args.host, args.filenames, activity_mode_override)
 
 
 def main():
-    filenames, activity_mode_override = eval_args()
-    access_token, user_id = login("/auth/login", "user1@example.com", "password1")
-    upload_all_files(filenames, user_id, access_token, activity_mode_override)
+    host, filenames, activity_mode_override = eval_args()
+    access_token, user_id = login(host, "/auth/login", "user1@example.com", "password1")
+    upload_all_files(host, filenames, user_id, access_token, activity_mode_override)
 
 
 if __name__ == "__main__":
